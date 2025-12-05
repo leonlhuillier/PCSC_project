ef48305d1e85cb3db7700953726a1a1c5b02ac46
fannata <fannata.doukasediko@epfl.ch>
2025-12-05 12:10:42 +0100

    architecture with cpp files

--- Source/Reader.cpp
+++ Source/Reader.cpp
@@ -1,3 +1,23 @@
 //
 // Created by 32484 on 05-12-25.
 //
+//
+// Created by fanni on 05/12/2025.
+//
+#include "Reader.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+
+// Constructor
+Reader::Reader(const std::string& fileName):mFilename(fileName) {
+    if (!FileExists()) {
+        std::cerr << "File" <<mFilename<< " does not exist!" << std::endl;
+    }
+}
+
+// Destructor
+Reader::~Reader() {
+}
+
+//
